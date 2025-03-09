45b8a30113306da2529b04b82dc18a9357a1ce51
Otaodesu <157286605+Otaodesu@users.noreply.github.com>
2025-03-09 22:21:58 +0900

    VOICEVOX CORE 0.16.0-preview.0に対応する

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # voicevox_flutter
 
-[voicevox_core](https://github.com/VOICEVOX/voicevox_core) の 非公式 ラッパーです。  
+Dart:FFIを利用して、VOICEVOX COREをArm64-v8aのAndroidデバイスで動かすFlutterパッケージ。  
+[voicevox_core](https://github.com/VOICEVOX/voicevox_core) の 非公式 ラッパーです。
 
 ## 使い方
+ご自身のFlutterアプリで使用する場合は以下のようにしてください。
 ### Android
-voicevox_coreのAndroid向けライブラリを[公式](https://github.com/VOICEVOX/voicevox_core/releases)からダウンロードし、`android/src/main/jniLibs/arm64-v8a/libvoicevox_core.so`を置き換えてください。
-### iOS
-voicevox_coreのiOS向けライブラリを[公式](https://github.com/VOICEVOX/voicevox_core/releases)からダウンロードし、`ios/Libraries`に`libonnxruntime.dylib`と`libvoicevox_core.dylib`を置いてください。また、`ios/voicevox_flutter.podspec`内の s.vendored_libraries を適切なパスに変更してください。
 
-自分のFlutterアプリで使用する場合は以下のようにしてください。
+- vvmモデルファイルを[ここ](https://github.com/VOICEVOX/voicevox_vvm/tree/main/vvms)からダウンロードし、`your_app/assets/model` フォルダに追加します。
 
-pubspec.yaml
+- voicevox_onnxruntimeのAndroid向けライブラリを[ここ](https://github.com/VOICEVOX/onnxruntime-builder/releases/tag/voicevox_onnxruntime-1.17.3)からダウンロードし、`your_app/assets/libvoicevox_onnxruntime.so` フォルダに追加します。
+
+- OpenJTalkのUTF-8辞書を[このへん](https://open-jtalk.sourceforge.net/)からダウンロードし、`your_app/assets/open_jtalk_dic_utf_8-1.11` フォルダに追加します。
+
+- `your_app/pubspec.yaml` を編集します。
+
 ```yaml
 dependencies:
   voicevox_flutter:
@@ -20,20 +24,24 @@ flutter:
   assets:
     - assets/open_jtalk_dic_utf_8-1.11/
     - assets/model/
-
+    - assets/libvoicevox_onnxruntime.so
 ```
 
-modelフォルダは公式ライブラリに同梱されています。
-openjtalkは[こちら](https://open-jtalk.sourceforge.net/)からダウンロードできます。
-
-実際の使用方法は[example](example)を参考にしてください。
+~~実際の使用方法は[example](example)を参考にしてください。~~  exampleは今のところ動きません😥
 
 ## 高レベルAPI
 VoicevoxFlutterクラスは現在audioQuery, synthesis, tts のみをサポートしています。
 
 ## 低レベルAPI
-[voicevox_flutter/generated_bindings.dart](lib/generated_bindings.dart)に[ffigen](https://github.com/dart-lang/ffigen)で生成しただけのものがあります。
+[generated_bindings.dart](lib/generated_bindings.dart)に[ffigen](https://github.com/dart-lang/ffigen)で生成しただけのものがあります。
 
 
 ## ライセンス
 MITライセンスが適用されています。[LICENSE](LICENSE)を参照してください。
+
+## ファイルの出どころ
+- [libc++_shared.so](android/src/main/jniLibs/arm64-v8a/libc++_shared.so): Android NDKの[ダウンロードページ](https://developer.android.com/ndk/downloads?hl=ja)に転がっていた `android-ndk-r27c-windows.zip` から拾いました。
+
+- [libvoicevox_core.so](android/src/main/jniLibs/libvoicevox_core.so): VOICEVOX CORE公式様の[Releases 0.16.0-preview.0](https://github.com/VOICEVOX/voicevox_core/releases/tag/0.16.0-preview.0)からありがたく頂戴いたしました。
+
+- [voicevox_core.h](voicevox_core.h): VOICEVOX CORE公式様の[Releases 0.16.0-preview.0](https://github.com/VOICEVOX/voicevox_core/releases/tag/0.16.0-preview.0)からありがたく頂戴いたしました。
--- lib/voicevox_flutter.dart
+++ lib/voicevox_flutter.dart
@@ -4,155 +4,356 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 
 import 'generated_bindings.dart';
-import 'voicevox_flutter_platform_interface.dart';
+
+// 参考文献😘: https://github.com/VOICEVOX/voicevox_core/blob/00f891c8664ed302a1b0778ed2eddea4551d6287/example/cpp/unix/simple_tts.cpp
 
 /// VoicevoxCoreLibraryのラッパークラス
 class VoicevoxFlutter extends VoicevoxCoreLibrary {
-  static final VoicevoxFlutter instance = VoicevoxFlutter._(DynamicLibrary.open(
-      Platform.isAndroid ? 'libvoicevox_core.so' : 'libvoicevox_core.dylib'));
+  static final VoicevoxFlutter instance = VoicevoxFlutter._(
+    Platform.isAndroid ? DynamicLibrary.open('libvoicevox_core.so') : DynamicLibrary.open('libvoicevox_core.dylib'),
+  );
   VoicevoxFlutter._(super.dynamicLibrary);
 
+  /// VoicevoxSynthesizerのポインタ
+  late Pointer<VoicevoxSynthesizer> _synthesizerPtr;
+
+  /// 読み込まれたモデルのポインタのリスト
+  final _loadedModelPtrList = <Pointer<VoicevoxVoiceModelFile>>[];
+
+  /// voicevox_flutterを初期化する
+  ///
+  /// [openJdkDictPath] OpenJtalkの辞書ファイルのパス
+  ///
+  /// [cpuNumThreads] CPUスレッド数
   Future<void> initialize({
-    required String modelPath,
     required String openJdkDictPath,
     int? cpuNumThreads,
-    bool loadAllModels = true,
   }) async {
-    await VoicevoxFlutterPlatform.instance.setModelPath(modelPath);
-    final option = voicevox_make_default_initialize_options();
-    final openJdkDictPathPtr = openJdkDictPath.toNativeUtf8();
-    option
-      ..acceleration_mode =
-          VoicevoxAccelerationMode.VOICEVOX_ACCELERATION_MODE_CPU
-      ..cpu_num_threads = cpuNumThreads ?? 0
-      ..load_all_models = loadAllModels
-      ..open_jtalk_dict_dir = openJdkDictPathPtr.cast<Char>();
-    final code = voicevox_initialize(option);
+    //TODO: onnxruntimeSoPathを引数に追加する
+    final onnxruntimeSoPath = '/data/user/0/com.example.voine/files/libvoicevox_onnxruntime.so';
 
-    malloc.free(openJdkDictPathPtr);
-    if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
-      throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+    final onnxruntimeSoPathPtr = onnxruntimeSoPath.toNativeUtf8();
+    final onnxruntimePtrPtr = malloc<Pointer<VoicevoxOnnxruntime>>();
+    final openJdkDictPathPtr = openJdkDictPath.toNativeUtf8();
+    final openJtalkPtrPtr = malloc<Pointer<OpenJtalkRc>>();
+    final outputSynthesizerPtrPtr = malloc<Pointer<VoicevoxSynthesizer>>();
+
+    try {
+      // onnxruntimeを準備する
+      final ortLoadOptions = voicevox_make_default_load_onnxruntime_options();
+      ortLoadOptions.filename = onnxruntimeSoPathPtr.cast<Char>();
+
+      final ortLoadResult = voicevox_onnxruntime_load_once(
+        ortLoadOptions,
+        onnxruntimePtrPtr,
+      );
+      if (ortLoadResult != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$ortLoadResult: ${errorcodeToText(ortLoadResult)}');
+      }
+
+      // OpenJTalkを準備する
+      final ojtLoadResult = voicevox_open_jtalk_rc_new(
+        openJdkDictPathPtr.cast<Char>(),
+        openJtalkPtrPtr,
+      );
+      if (ojtLoadResult != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$ojtLoadResult: ${errorcodeToText(ojtLoadResult)}');
+      }
+
+      // VOICEVOX_COREを起動する！！
+      final initializeOptions = voicevox_make_default_initialize_options();
+
+      initializeOptions
+        ..acceleration_mode = VoicevoxAccelerationMode.VOICEVOX_ACCELERATION_MODE_CPU
+        ..cpu_num_threads = cpuNumThreads ?? 0;
+
+      final voicevoxInitResult = voicevox_synthesizer_new(
+        onnxruntimePtrPtr.value,
+        openJtalkPtrPtr.value,
+        initializeOptions,
+        outputSynthesizerPtrPtr,
+      );
+      if (voicevoxInitResult != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX CORE エラー$voicevoxInitResult: ${errorcodeToText(voicevoxInitResult)}');
+      }
+
+      _synthesizerPtr = outputSynthesizerPtrPtr.value;
+    } catch (_) {
+      rethrow;
+    } finally {
+      calloc.free(openJdkDictPathPtr);
+      calloc.free(onnxruntimeSoPathPtr);
+      malloc
+        ..free(onnxruntimePtrPtr)
+        ..free(openJtalkPtrPtr)
+        ..free(outputSynthesizerPtrPtr);
     }
   }
 
-  /// AudioQuery を実行する
+  /// モデルを読み込む
+  ///
+  /// [modelPath] vvmファイルのパス
+  void loadVoiceModel(String modelPath) {
+    final modelPtrPtr = malloc<Pointer<VoicevoxVoiceModelFile>>();
+    final modelPathPtr = modelPath.toNativeUtf8();
+    try {
+      // モデルファイルをopenしていく
+      final modelOpenResult = voicevox_voice_model_file_open(
+        modelPathPtr.cast<Char>(),
+        modelPtrPtr,
+      );
+      if (modelOpenResult != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$modelOpenResult: ${errorcodeToText(modelOpenResult)}');
+      }
+
+      // モデルファイルをloadしていく… openで手札に取って、loadで場に出す的な？カードゲーム的思想やな
+      final modelLoadResult = voicevox_synthesizer_load_voice_model(
+        _synthesizerPtr,
+        modelPtrPtr.value,
+      );
+
+      if (modelLoadResult != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$modelLoadResult: ${errorcodeToText(modelLoadResult)}');
+      }
+      _loadedModelPtrList.add(modelPtrPtr.value);
+    } finally {
+      calloc.free(modelPathPtr);
+      malloc.free(modelPtrPtr);
+    }
+  }
+
+  /// テキストから AudioQuery を生成する
   ///
   /// [text] テキスト
   ///
-  /// [speakerId] 話者ID
+  /// [styleId] スタイルID
   ///
   /// [kana] aquestalk形式のkanaとしてテキストを解釈する
   String audioQuery(
     String text, {
-    required int speakerId,
+    required int styleId,
     bool kana = false,
   }) {
-    final defaultQueryOption = voicevox_make_default_audio_query_options();
-    defaultQueryOption.kana = kana;
-    Pointer<Pointer<Char>> outputPtr = calloc<Pointer<Char>>();
-    final code = voicevox_audio_query(
-      text.toNativeUtf8().cast<Char>(),
-      speakerId,
-      defaultQueryOption,
-      outputPtr,
-    );
-    if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
-      throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+    final textPtr = text.toNativeUtf8();
+    Pointer<Pointer<Char>> outputPtr = malloc<Pointer<Char>>();
+    try {
+      late int code;
+      if (kana) {
+        code = voicevox_synthesizer_create_audio_query_from_kana(
+          _synthesizerPtr,
+          textPtr.cast<Char>(),
+          styleId,
+          outputPtr,
+        );
+      } else {
+        code = voicevox_synthesizer_create_audio_query(
+          _synthesizerPtr,
+          textPtr.cast<Char>(),
+          styleId,
+          outputPtr,
+        );
+      }
+
+      if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+      }
+      final query = outputPtr.value.cast<Utf8>().toDartString();
+      return query;
+    } catch (_) {
+      rethrow;
+    } finally {
+      calloc.free(textPtr);
+      malloc.free(outputPtr);
     }
-    final query = outputPtr.value.cast<Utf8>().toDartString();
-    calloc.free(outputPtr);
-    return query;
+  }
+
+  /// 増やす予定。テキストから AudioQuery を生成する
+  ///
+  /// [text] テキスト
+  ///
+  /// [styleId] スタイルID
+  String textToAudioQuery(
+    String text, {
+    required int styleId,
+  }) {
+    final textPtr = text.toNativeUtf8();
+    Pointer<Pointer<Char>> outputPtr = malloc<Pointer<Char>>();
+    try {
+      final resultCode = voicevox_synthesizer_create_audio_query(
+        _synthesizerPtr,
+        textPtr.cast<Char>(),
+        styleId,
+        outputPtr,
+      );
+      if (resultCode != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$resultCode: ${errorcodeToText(resultCode)}');
+      }
+
+      final query = outputPtr.value.cast<Utf8>().toDartString();
+      return query;
+    } catch (_) {
+      rethrow;
+    } finally {
+      calloc.free(textPtr);
+      malloc.free(outputPtr);
+    }
   }
 
   /// AudioQuery から音声合成する
   ///
   /// [query] jsonフォーマットされた AudioQuery
   ///
-  /// [speakerId] 話者ID
+  /// [styleId] スタイルID
   ///
   /// [outputPath] 出力ファイルパス
   void synthesis(
     String query, {
-    required int speakerId,
+    required int styleId,
     required String outputPath,
     bool enableInterrogativeUpspeak = true,
   }) {
-    final synthesisOptions = voicevox_make_default_synthesis_options();
-    synthesisOptions.enable_interrogative_upspeak = enableInterrogativeUpspeak;
-    final Pointer<UintPtr> outputWavLengthPtr = calloc<UintPtr>();
-    Pointer<Pointer<Uint8>> outputWavPtr = calloc<Pointer<Uint8>>();
-    final queryPtr = query.toNativeUtf8();
-    final code = voicevox_synthesis(
-      queryPtr.cast<Char>(),
-      speakerId,
-      synthesisOptions,
-      outputWavLengthPtr,
-      outputWavPtr,
-    );
-    if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
-      throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+    final outputWavLengthPtr = malloc<UintPtr>();
+    final outputWavPtr = malloc<Pointer<Uint8>>();
+    try {
+      final synthesisOptions = voicevox_make_default_synthesis_options();
+      synthesisOptions.enable_interrogative_upspeak =
+          enableInterrogativeUpspeak; // generated_bindingsを再生成するとなぜかInt(大文字)になったのでEDITした👌
+
+      final queryPtr = query.toNativeUtf8();
+      final code = voicevox_synthesizer_synthesis(
+        _synthesizerPtr,
+        queryPtr.cast<Char>(),
+        styleId,
+        synthesisOptions,
+        outputWavLengthPtr, // generated_bindingsを再生成するとなぜかInt(大文字)になったのでEDITした👌
+        outputWavPtr,
+      );
+      if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+      }
+      final wavFile = File(outputPath);
+      wavFile.writeAsBytesSync(outputWavPtr.value.asTypedList(outputWavLengthPtr.value));
+    } catch (_) {
+      rethrow;
+    } finally {
+      malloc
+        ..free(outputWavLengthPtr)
+        ..free(outputWavPtr);
     }
-    final wavFile = File(outputPath);
+  }
 
-    wavFile.writeAsBytesSync(
-        outputWavPtr.value.asTypedList(outputWavLengthPtr.value));
+  /// 増やす予定。 AudioQuery から音声合成する
+  ///
+  /// [query] jsonフォーマットされた AudioQuery
+  ///
+  /// [styleId] スタイルID
+  ///
+  /// [outputPath] 出力ファイルパス
+  void audioQueryToWav(
+    String query, {
+    required int styleId,
+    required String outputPath,
+    bool enableInterrogativeUpspeak = true,
+  }) {
+    final outputWavLengthPtr = malloc<UintPtr>();
+    final outputWavPtr = malloc<Pointer<Uint8>>();
+    try {
+      final synthesisOptions = voicevox_make_default_synthesis_options();
+      synthesisOptions.enable_interrogative_upspeak = enableInterrogativeUpspeak;
 
-    calloc.free(outputWavLengthPtr);
-    calloc.free(outputWavPtr);
+      final queryPtr = query.toNativeUtf8();
+      final resultCode = voicevox_synthesizer_synthesis(
+        _synthesizerPtr,
+        queryPtr.cast<Char>(),
+        styleId,
+        synthesisOptions,
+        outputWavLengthPtr,
+        outputWavPtr,
+      );
+      if (resultCode != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VOICEVOX_CORE エラー$resultCode: ${errorcodeToText(resultCode)}');
+      }
+      final wavFile = File(outputPath);
+      wavFile.writeAsBytesSync(outputWavPtr.value.asTypedList(outputWavLengthPtr.value));
+    } catch (_) {
+      rethrow;
+    } finally {
+      malloc
+        ..free(outputWavLengthPtr)
+        ..free(outputWavPtr);
+    }
   }
 
   /// テキスト音声合成を実行する
   ///
   /// [text] テキスト
   ///
-  /// [speakerId] 話者ID
+  /// [styleId] スタイルID
   ///
   /// [outputPath] 出力ファイルパス
   ///
-  /// [kana] aquestalk形式のkanaとしてテキストを解釈する
+  /// [kana] AquesTalk形式のkanaとしてテキストを解釈する
   ///
   /// [enableInterrogativeUpspeak] 疑問文の調整を有効にする
   String tts(
     String text, {
-    required int speakerId,
+    required int styleId,
     required String outputPath,
     bool kana = false,
     bool enableInterrogativeUpspeak = true,
   }) {
-    final Pointer<UintPtr> outputWavLengthPtr = calloc<UintPtr>();
-
-    Pointer<Pointer<Uint8>> outputWavPtr = calloc<Pointer<Uint8>>();
-    final option = voicevox_make_default_tts_options();
-    option
-      ..kana = kana
-      ..enable_interrogative_upspeak = enableInterrogativeUpspeak;
-
+    final outputWavLengthPtr = malloc<UintPtr>();
+    final outputWavPtr = malloc<Pointer<Uint8>>();
     final textPtr = text.toNativeUtf8();
-    final code = voicevox_tts(textPtr.cast<Char>(), speakerId, option,
-        outputWavLengthPtr, outputWavPtr);
+    try {
+      final option = voicevox_make_default_tts_options();
+      option.enable_interrogative_upspeak = enableInterrogativeUpspeak;
+      late int code;
+      if (kana) {
+        code = voicevox_synthesizer_tts_from_kana(
+          _synthesizerPtr,
+          textPtr.cast<Char>(),
+          styleId,
+          option,
+          outputWavLengthPtr,
+          outputWavPtr,
+        );
+      } else {
+        code = voicevox_synthesizer_tts(
+          _synthesizerPtr,
+          textPtr.cast<Char>(),
+          styleId,
+          option,
+          outputWavLengthPtr,
+          outputWavPtr,
+        );
+      }
+      if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
+        throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+      }
+      final wavFile = File(outputPath);
 
-    if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
-      throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+      wavFile.writeAsBytesSync(outputWavPtr.value.asTypedList(outputWavLengthPtr.value));
+
+      malloc.free(textPtr);
+      malloc.free(outputWavLengthPtr);
+      malloc.free(outputWavPtr);
+      return outputPath;
+    } catch (_) {
+      rethrow;
+    } finally {
+      malloc
+        ..free(outputWavLengthPtr)
+        ..free(outputWavPtr);
+      calloc.free(textPtr);
     }
-    final wavFile = File(outputPath);
-
-    wavFile.writeAsBytesSync(
-        outputWavPtr.value.asTypedList(outputWavLengthPtr.value));
-
-    malloc.free(textPtr);
-    calloc.free(outputWavLengthPtr);
-    calloc.free(outputWavPtr);
-    return outputPath;
   }
 
-  /// モデルを読み込む
-  ///
-  /// [id] 読み込むモデルの話者ID
-  void loadModel(int id) {
-    final code = voicevox_load_model(id);
-    if (code != VoicevoxResultCode.VOICEVOX_RESULT_OK) {
-      throw Exception('VoicevoxResult: ${errorcodeToText(code)}');
+  void dispose() {
+    for (final modelPtr in _loadedModelPtrList) {
+      voicevox_voice_model_file_delete(modelPtr);
     }
+    voicevox_synthesizer_delete(_synthesizerPtr);
   }
 }
 
@@ -163,30 +364,52 @@ String errorcodeToText(int code) {
       return '成功';
     case 1:
       return 'open_jtalk辞書ファイルが読み込まれていない';
-    case 2:
-      return 'modelの読み込みに失敗した';
     case 3:
       return 'サポートされているデバイス情報取得に失敗した';
     case 4:
       return 'GPUモードがサポートされていない';
-    case 5:
-      return 'メタ情報読み込みに失敗した';
     case 6:
-      return 'ステータスが初期化されていない';
+      return 'スタイルIDに対するスタイルが見つからなかった';
     case 7:
-      return '無効なspeaker_idが指定された';
+      return '音声モデルIDに対する音声モデルが見つからなかった';
     case 8:
-      return '無効なmodel_indexが指定された';
-    case 9:
       return '推論に失敗した';
-    case 10:
-      return 'コンテキストラベル出力に失敗した';
     case 11:
-      return '無効なutf8文字列が入力された';
+      return '入力テキストの解析に失敗した';
     case 12:
-      return 'aquestalk形式のテキストの解析に失敗した';
+      return '無効なutf8文字列が入力された';
     case 13:
+      return 'AquesTalk風記法のテキストの解析に失敗した';
+    case 14:
       return '無効なAudioQuery';
+    case 15:
+      return '無効なAccentPhrase';
+    case 16:
+      return 'ZIPファイルを開くことに失敗した';
+    case 17:
+      return 'ZIP内のファイルが読めなかった';
+    case 18:
+      return 'すでに読み込まれている音声モデルを読み込もうとした';
+    case 20:
+      return 'ユーザー辞書を読み込めなかった';
+    case 21:
+      return 'ユーザー辞書を書き込めなかった';
+    case 22:
+      return 'ユーザー辞書に単語が見つからなかった';
+    case 23:
+      return 'OpenJTalkのユーザー辞書の設定に失敗した';
+    case 24:
+      return 'ユーザー辞書の単語のバリデーションに失敗した';
+    case 25:
+      return 'UUIDの変換に失敗した';
+    case 26:
+      return 'すでに読み込まれているスタイルを読み込もうとした';
+    case 27:
+      return '無効なモデルデータ';
+    case 28:
+      return 'モデルの形式が不正';
+    case 29:
+      return '推論ライブラリのロードまたは初期化ができなかった';
     default:
       return '不明なエラー';
   }
